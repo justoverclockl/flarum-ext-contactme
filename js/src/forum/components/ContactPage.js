9468f98ba4041f3ad75d7674f657c6a162224606
Justoverclock <info@justoverclock.it>
2021-04-21 13:45:25 +0200

    English translation

--- js/src/forum/components/ContactPage.js
+++ js/src/forum/components/ContactPage.js
@@ -3,7 +3,7 @@
  *  Author: Marco Colia
  *  Website: https://flarum.it
  *
- *  Special Thanks to Sasha Askvortsov
+ *  Special Thanks to Askvortsov
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
